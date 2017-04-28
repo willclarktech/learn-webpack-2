ef43ac6e0bc77b5dead0b66ec7afaeb192d82cb8
willclarktech <will@willclark.tech>
2017-04-28 16:26:04 +0200

    add async import

--- src/extra.js
+++ src/extra.js
@@ -1,5 +1,10 @@
-import moment from 'moment'
+loadMoment = async () => {
+  console.time('test')
+  const moment = await import('moment')
+  const m = moment().format('MMMM Do YYYY, h:mm:ss a')
+  console.log(m)
+  console.timeEnd('test')
+}
 
-const m = moment()
-
-console.log(m)
+document.getElementById('button1').addEventListener('click', loadMoment)
+document.getElementById('button2').addEventListener('click', loadMoment)
